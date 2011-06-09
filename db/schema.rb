d622cf81f185f7a55003180725a9020a2545b273
Angel Romero <angelo102@gmail.com>
2011-06-09 00:33:16 -0600

    Task E! Playtime 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110513070725) do
+ActiveRecord::Schema.define(:version => 20110609044504) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20110513070725) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",   :default => 1
+    t.decimal  "price"
   end
 
   create_table "products", :force => true do |t|
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -54,4 +54,7 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
+  
+
 end
